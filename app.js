82082bcc73db8bef69d751a062692268171bbf19
swanalex <swanalex@oregonstate.edu>
2024-07-30 13:05:49 -0400

    Setup app.js and GET route

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-console.log("hello world")
-console.log("goodbye world")
+import express from 'express'
+import { getRecipes, getRecipe, updateRecipe, deleteRecipe } from './database.js'
+
+const app = express()
+app.use(express.json())
+
+// Routes
+app.get("/recipes", async (req, res) => {
+    const recipes = await getRecipes()
+    res.render("recipes.ejs", { recipes })
+})
+
+
+
+// Error Logging
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send("Oops! Something went wrong!")
+})
+
+app.use(express.static("public"))
+
+app.listen(5467, () => {
+    console.log("Server is running on port 5467")
+})
